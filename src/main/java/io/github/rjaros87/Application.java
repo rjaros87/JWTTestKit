5b604a3a1c624a35df8239d03cb432c819dd25a9
Radoslaw Jaros <rjaros87@users.noreply.github.com>
2025-02-16 00:09:04 +0100

    Added Json appender in logback, added labels to docker image.

--- src/main/java/io/github/rjaros87/Application.java
+++ src/main/java/io/github/rjaros87/Application.java
@@ -30,6 +30,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 )
 public class Application {
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        Micronaut
+            .build(args)
+            .mainClass(Application.class)
+            .banner(false)
+            .start();
     }
 }
